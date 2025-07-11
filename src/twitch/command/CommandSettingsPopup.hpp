da27c9e8825be60d147a61c36eac2c1583cff8dc
Cheeseworks <bluewitherer2422@gmail.com>
2025-07-11 15:44:53 -0500

    avoid macos warning

--- src/twitch/command/CommandSettingsPopup.hpp
+++ src/twitch/command/CommandSettingsPopup.hpp
@@ -15,18 +15,25 @@ protected:
     TextInput* m_notificationInput = nullptr;
     CCMenuItemToggler* m_killPlayerCheckbox = nullptr;
     TwitchCommand m_command = TwitchCommand("", "", "", 0, {});
+
     bool setup(TwitchCommand command) override;
     void onSave(cocos2d::CCObject* sender);
     void onCloseBtn(cocos2d::CCObject* sender);
 
 public:
     std::unordered_map<int, std::string> m_notificationActionTexts;
+
     void updateNotificationNextTextLabel(int actionIdx, const std::string& nextText);
+
     static CommandSettingsPopup* create(TwitchCommand command);
+
     std::string getNotificationText() const;
     std::vector<std::string> m_commandActions;
+
     cocos2d::CCNode* m_actionContent = nullptr;
+
     float m_actionSectionHeight = 0.f;
+
     void refreshActionsList();
     void onAddEventAction(cocos2d::CCObject* sender);
     void onRemoveAction(cocos2d::CCObject* sender);
